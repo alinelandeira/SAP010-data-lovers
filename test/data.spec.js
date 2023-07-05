1651bc072f242adaa351153a0994cdad38d2a26d
CatarinaEudoxia <ceudoxia.ferreira@gmail.com>
2023-07-05 01:54:51 -0300

    Calculo agregado, testes e responsividade finalizados. Inicio do desenvolvimento no readme

--- test/data.spec.js
+++ test/data.spec.js
@@ -6,15 +6,15 @@ describe('pokeType', () => {
   });
 
   it('deve retornar o tipo "todos"', () => {
-    const pokArray = [{ type: 'fire' }, { type: 'water' }, { type: 'grass' }];
+    const pokeArray = [{ type: 'fire' }, { type: 'water' }, { type: 'grass' }];
     const todos = 'allPoke';
-    expect(pokeType(pokArray, todos)).toEqual(pokArray);
+    expect(pokeType(pokeArray, todos)).toEqual(pokeArray);
   });
 
   it('deve retornar o tipo filtrado', () => {
-    const pokArray = [{ type: 'fire' }, { type: 'water' }, { type: 'grass' }];
-    const pokChage = 'fire';
-    expect(pokeType(pokArray, pokChage)).toEqual([{ type: 'fire' }]);
+    const pokeArray = [{ type: 'fire' }, { type: 'water' }, { type: 'grass' }];
+    const pokeChange = 'fire';
+    expect(pokeType(pokeArray, pokeChange)).toEqual([{ type: 'fire' }]);
   });
 });
 
@@ -24,7 +24,7 @@ describe('pokeOrder', () => {
   });
 
   it('deve retornar nomes em ordem crescente', () => {
-    const poArray = [
+    const pokeArray = [
       { 'num': '063', 'name': 'Abra' },
       { 'num': '142', 'name': 'Aerodactyl' },
       { 'num': '190', 'name': 'Aipom' },
@@ -34,8 +34,8 @@ describe('pokeOrder', () => {
       { 'num': '059', 'name': 'Arcanine' },
       { 'num': '168', 'name': 'Ariados' }
     ];
-    const poChange = "nameAsc";
-    expect(pokeOrder(poArray, poChange)).toEqual([
+    const pokeChange = "nameAsc";
+    expect(pokeOrder(pokeArray, pokeChange)).toEqual([
       { 'num': '063', 'name': 'Abra' },
       { 'num': '142', 'name': 'Aerodactyl' },
       { 'num': '190', 'name': 'Aipom' },
@@ -48,7 +48,7 @@ describe('pokeOrder', () => {
   });
 
   it('deve retornar nomes em ordem decrescente', () => {
-    const poArray = [
+    const pokeArray = [
       { 'num': '063', 'name': 'Abra' },
       { 'num': '142', 'name': 'Aerodactyl' },
       { 'num': '190', 'name': 'Aipom' },
@@ -58,8 +58,8 @@ describe('pokeOrder', () => {
       { 'num': '059', 'name': 'Arcanine' },
       { 'num': '168', 'name': 'Ariados' }
     ];
-    const poNChange = "nameDesc";
-    expect(pokeOrder(poArray, poNChange)).toEqual([
+    const pokeNumberChange = "nameDesc";
+    expect(pokeOrder(pokeArray, pokeNumberChange)).toEqual([
       { 'num': '168', 'name': 'Ariados' },
       { 'num': '059', 'name': 'Arcanine' },
       { 'num': '024', 'name': 'Arbok' },
@@ -72,7 +72,7 @@ describe('pokeOrder', () => {
   });
 
   it('deve retornar números em ordem crescente', () => {
-    const poArray = [
+    const pokeArray = [
       { 'num': '063', 'name': 'Abra' },
       { 'num': '142', 'name': 'Aerodactyl' },
       { 'num': '190', 'name': 'Aipom' },
@@ -82,8 +82,8 @@ describe('pokeOrder', () => {
       { 'num': '059', 'name': 'Arcanine' },
       { 'num': '168', 'name': 'Ariados' }
     ];
-    const poNuChange = "numberAsc";
-    expect(pokeOrder(poArray, poNuChange)).toEqual([
+    const pokeNumberChange = "numberAsc";
+    expect(pokeOrder(pokeArray, pokeNumberChange)).toEqual([
       { 'num': '024', 'name': 'Arbok' },
       { 'num': '059', 'name': 'Arcanine' },
       { 'num': '063', 'name': 'Abra' },
@@ -96,7 +96,7 @@ describe('pokeOrder', () => {
   });
 
   it('deve retornar números em ordem decrescente', () => {
-    const poArray = [
+    const pokeArray = [
       { 'num': '063', 'name': 'Abra' },
       { 'num': '142', 'name': 'Aerodactyl' },
       { 'num': '190', 'name': 'Aipom' },
@@ -106,8 +106,8 @@ describe('pokeOrder', () => {
       { 'num': '059', 'name': 'Arcanine' },
       { 'num': '168', 'name': 'Ariados' }
     ];
-    const poNuDChange = "numberDesc";
-    expect(pokeOrder(poArray, poNuDChange)).toEqual([
+    const pokeNumberChange = "numberDesc";
+    expect(pokeOrder(pokeArray, pokeNumberChange)).toEqual([
       { 'num': '190', 'name': 'Aipom' },
       { 'num': '181', 'name': 'Ampharos' },
       { 'num': '168', 'name': 'Ariados' },
@@ -125,54 +125,31 @@ describe('percentual', () => {
     expect(typeof percentual).toBe('function');
   });
 
-  it('Deve retornar a porcentagem de pokémons por cada tipo', () => {
-    const poArray = [
-    { 'type': 'normal' },
-    { 'type': 'fire' },
-    { 'type': 'water' },
-    { 'type': 'grass' },
-    { 'type': 'electric' },
-    { 'type': 'bug' },
-    { 'type': 'ground' },
-    { 'type': 'poison' },
-    { 'type': 'fighting' },
-    { 'type': 'psychic' },
-    { 'type': 'rock' },
-    { 'type': 'flying' },
-    { 'type': 'ghost' },
-    { 'type': 'ice' },
-    { 'type': 'dragon' },
-    { 'type': 'steel' },
-    { 'type': 'dark' },
-    { 'type': 'fairy' }
-  ];
+  it('deve retornar a porcentagem de pokémons por cada tipo em relação ao total', () => {
+    const allTypes = [
+      { 'num': '000', 'name': 'Miltank', 'type': 'normal' },
+      { 'num': '142', 'name': 'Jolteon', 'type': 'electric' },
+      { 'num': '190', 'name': 'Aipom', 'type': 'normal' },
+      { 'num': '065', 'name': 'Alakazam', 'type': 'psychic' },
+      { 'num': '181', 'name': 'Ampharos', 'type': 'electric' },
+      { 'num': '024', 'name': 'Ditto', 'type': 'normal' },
+      { 'num': '059', 'name': 'Jigglypuff', 'type': 'normal' },
+      { 'num': '168', 'name': 'Raikou', 'type': 'electric' },
+      { 'num': '045', 'name': 'Abra', 'type': 'psychic' },
+      { 'num': '164', 'name': 'Lugia', 'type': 'psychic' }
+    ];
 
-  const tipoNormal = tiposPokemon.filter(pokemon => pokemon.type === 'normal');
-  const tipoFire = tiposPokemon.filter(pokemon => pokemon.type === 'fire');
-  const tipoWater = tiposPokemon.filter(pokemon => pokemon.type === 'water');
-  const tipoGrass = tiposPokemon.filter(pokemon => pokemon.type === 'grass');
-  const tipoElectric = tiposPokemon.filter(pokemon => pokemon.type === 'electric');
-  const tipoBug = tiposPokemon.filter(pokemon => pokemon.type === 'bug');
-  const tipoGround = tiposPokemon.filter(pokemon => pokemon.type === 'ground');
-  const tipoPoison = tiposPokemon.filter(pokemon => pokemon.type === 'poison');
-  const tipoFighting = tiposPokemon.filter(pokemon => pokemon.type === 'fighting');
-  const tipoPsychic = tiposPokemon.filter(pokemon => pokemon.type === 'psychic');
-  const tipoRock = tiposPokemon.filter(pokemon => pokemon.type === 'rock');
-  const tipoFlying = tiposPokemon.filter(pokemon => pokemon.type === 'flying');
-  const tipoGhost = tiposPokemon.filter(pokemon => pokemon.type === 'ghost');
-  const tipoIce = tiposPokemon.filter(pokemon => pokemon.type === 'ice');
-  const tipoDragon = tiposPokemon.filter(pokemon => pokemon.type === 'dragon');
-  const tipoSteel = tiposPokemon.filter(pokemon => pokemon.type === 'steel');
-  const tipoDark = tiposPokemon.filter(pokemon => pokemon.type === 'dark');
-  const tipoFairy = tiposPokemon.filter(pokemon => pokemon.type === 'fairy');
+    const typeNormal = allTypes.filter(pokemon => pokemon.type === 'normal');
+    const typeElectric = allTypes.filter(pokemon => pokemon.type === 'electric');
+    const typePsychic = allTypes.filter(pokemon => pokemon.type === 'psychic');
 
-    const resultadoFramboesa = percentual(poTipoFramboesa.length, poArray.length);
-    const resultadoAmor = percentual(poTipoAmor.length, poArray.length);
-    const resultadoAmora = percentual(poTipoAmora.length, poArray.length);
+    const normalResult = percentual(typeNormal.length, allTypes.length);
+    const electricResult = percentual(typeElectric.length, allTypes.length);
+    const psychicResult = percentual(typePsychic.length, allTypes.length);
 
-    expect(resultadoFramboesa).toEqual(30);
-    expect(resultadoAmor).toEqual(30);
-    expect(resultadoAmora).toEqual(50);
+    expect(normalResult).toEqual(40);
+    expect(electricResult).toEqual(30);
+    expect(psychicResult).toEqual(30);
   });
 });
 
